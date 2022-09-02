447c83cb519338f365456bf58db8f02afdaae1b6
Gemmuel <gemmueldelacruz@gmail.com>
2022-09-02 21:03:22 +0800

    added movie sections

---
+++ movies/movie.js
@@ -0,0 +1,51 @@
+//  Video section 1 //
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    pagination: {
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".next-button",
+    prevEl: ".prev-button",
+    },
+});
+
+//  Video section 2 //
+
+var swiper = new Swiper(".mySwiper-two", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    pagination: {
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".next-button-two",
+    prevEl: ".prev-button-two",
+    },
+});
+
+//  Video section 3 //
+
+var swiper = new Swiper(".mySwiper-three", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    pagination: {
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".next-button-three",
+    prevEl: ".prev-button-three",
+    },
+});
+
